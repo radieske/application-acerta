caa929fc0869dfe7007db04280ce072cc346aeb9
Joanthan Radieske <mrdjona@gmail.com>
2022-02-09 22:01:27 -0300

    adicionado api fake

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333/'
+    baseURL: 'https://api-application-acerta.herokuapp.com/'
 });
 
 export default api;
